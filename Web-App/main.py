5cdae2ba6cd01b23e53c795c23998916e69db593
Younes-Barkat <younesbarkat46@gmail.com>
2026-03-08 16:34:01 +0100

    added about.html

--- Web-App/main.py
+++ Web-App/main.py
@@ -8,6 +8,10 @@ app.secret_key = "bill_wise_app"
 def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
 
 
 if __name__ == "__main__" :
